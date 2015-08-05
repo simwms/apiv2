b7e17af35e2a2220e39434fbb3ddea6b3f91503e
Thomas Chen <foxnewsnetwork@gmail.com>
2015-08-05 12:14:43 -0700

    fixed asset deployment strategy and we no longer build assets on heroku

--- brunch-config.js
+++ brunch-config.js
@@ -1,29 +1,8 @@
 exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
-    javascripts: {
-      joinTo: 'js/app.js'
-      // To use a separate vendor.js bundle, specify two files path
-      // https://github.com/brunch/brunch/blob/stable/docs/config.md#files
-      // joinTo: {
-      //  'js/app.js': /^(web\/static\/js)/,
-      //  'js/vendor.js': /^(web\/static\/vendor)/
-      // }
-      //
-      // To change the order of concatenation of files, explictly mention here
-      // https://github.com/brunch/brunch/tree/master/docs#concatenation
-      // order: {
-      //   before: [
-      //     'web/static/vendor/js/jquery-2.1.1.js',
-      //     'web/static/vendor/js/bootstrap.min.js'
-      //   ]
-      // }
-    },
     stylesheets: {
       joinTo: 'css/app.css'
-    },
-    templates: {
-      joinTo: 'js/app.js'
     }
   },
 
@@ -45,10 +24,6 @@ exports.config = {
 
   // Configure your plugins
   plugins: {
-    babel: {
-      // Do not use ES6 compiler in vendor code
-      ignore: [/^(web\/static\/vendor)/]
-    },
     sass: {
       options: {
         includePaths: [
